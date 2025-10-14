7d6885d84f0d701e072b8739ff5e60e721954712
StevenJohnWeaver <stevenjweaver@gmail.com>
2025-10-14 14:23:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -87,11 +87,35 @@ locals {
   lb_subnets = slice(data.aws_subnets.default_vpc_subnets.ids, 0, 2)
 }
 
+resource "aws_security_group" "lb_sg" {
+  name        = "app-lb-sg"
+  description = "Security group for the application load balancer"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "app-lb-sg"
+  }
+}
+
 resource "aws_lb" "app_lb" {
   name               = "app-load-balancer"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = []
+  security_groups    = [aws_security_group.lb_sg.id]
   subnets            = local.lb_subnets
   tags = {
     Environment = "Demo"
